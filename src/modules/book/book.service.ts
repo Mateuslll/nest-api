51e524d47279ee56acd17a65ac8f0b29c0306127
gellado <lima.mt96@gmail.com>
2024-08-05 13:01:36 -0300

    Books Api with prisma and nest.js

---
+++ src/modules/book/book.service.ts
@@ -0,0 +1,57 @@
+import { Injectable } from '@nestjs/common';
+import { BookDto } from './book.dto';
+import { PrismaService } from 'src/database/PrismaService';
+
+@Injectable()
+export class BookService {
+  constructor(private prisma: PrismaService) {}
+
+  async create(data: BookDto) {
+    const bookExists = await this.prisma.book.findFirst({
+      where: {
+        bar_code: data.bar_code,
+      },
+    });
+    if (bookExists) {
+      throw new Error('Book already exists');
+    }
+    const book = await this.prisma.book.create({
+      data,
+    });
+    return book;
+  }
+  async findAll() {
+    return this.prisma.book.findMany();
+  }
+  async update(id: string, data: BookDto) {
+    const bookExists = await this.prisma.book.findUnique({
+      where: {
+        id,
+      },
+    });
+    if (!bookExists) {
+      throw new Error('Book does not exist');
+    }
+    return await this.prisma.book.update({
+      data,
+      where: {
+        id,
+      },
+    });
+  }
+  async delete(id: string) {
+    const bookExists = await this.prisma.book.findUnique({
+      where: {
+        id,
+      },
+    });
+    if (!bookExists) {
+      throw new Error('Book does not exist');
+    }
+    return await this.prisma.book.delete({
+      where: {
+        id,
+      },
+    });
+  }
+}
